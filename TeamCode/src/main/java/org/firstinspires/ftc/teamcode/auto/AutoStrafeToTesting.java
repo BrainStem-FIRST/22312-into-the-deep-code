45ada46f7b61eca960242921774c16c5fdcf3f79
Justin16Chen <justin.chen0160@gmail.com>
2024-12-05 22:29:24 -0500

    added strafe testing autos

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AutoStrafeToTesting.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.driveTrain.PinpointDrive;
+import org.firstinspires.ftc.teamcode.robot.AllianceColor;
+import org.firstinspires.ftc.teamcode.robot.BrainSTEMRobot;
+
+@Autonomous
+@Config
+public class AutoStrafeToTesting extends LinearOpMode {
+    public static class Params {
+        public double x1 = -48;
+        public double y1 = -60;
+        public double h1 = 0;
+        public double x2 = 48;
+        public double y2 = -32;
+    }
+    public static Params params = new Params();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+
+        Pose2d beginPose = new Pose2d(params.x1, params.y1, params.h1);
+        Vector2d pos1 = new Vector2d(params.x2, params.y2);
+
+        BrainSTEMRobot robot = new BrainSTEMRobot(hardwareMap, telemetry, AllianceColor.RED, beginPose);
+        PinpointDrive drive = robot.getDriveTrain();
+
+        Action driveStrafe = drive.actionBuilder(beginPose)
+                .strafeTo(pos1)
+                .build();
+
+        waitForStart();
+
+        Actions.runBlocking(new SequentialAction(
+                driveStrafe
+        ));
+    }
+}
